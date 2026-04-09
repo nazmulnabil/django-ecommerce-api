d820ba932f6c37129c51cb6b695b86282aa2ad0d
NKNabil1995 <nazmul.karim@brainstation-23.com>
2026-04-09 18:53:47 +0600

    feat:create core app

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HealthCheckView(APIView):
+    def get(self, request):
+        return Response({
+            "status": "success",
+            "message": "The Django backend is running perfectly!"
+        })
