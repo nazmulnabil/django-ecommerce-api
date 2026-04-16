f18aa73157c2515b0c7e90ab57e82f425a515d61
Nazmul Karim Nabil <nabilnazmul868@gmail.com>
2026-04-16 07:29:29 +0600

    feat: register category and product models to admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'is_active')
+    list_filter = ('is_active', 'category')
